0cfb9e88a42d736bfe673b784455f18dda230e93
yorozc <143294600+yorozc@users.noreply.github.com>
2025-04-18 03:09:21 -0700

    got rid of testing code for button.py

--- button.py
+++ button.py
@@ -1,16 +1,6 @@
 import pygame
 # will be used to create and test buttons
 
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 800
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Button Demo')
-
-#load button images
-start_img = pygame.image.load('resources/start_btn.png').convert_alpha()
-exit_img = pygame.image.load('resources/exit_btn.png').convert_alpha()
-
 class Button:
     def __init__(self, x, y, image, scale): # (x,y) determines pos, image = img path, scale = how big or small
         width = image.get_width()
@@ -37,29 +27,3 @@ class Button:
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
-
-    
-'''
-start_button = Button(100, 200, start_img, 0.8)
-exit_button = Button(450, 200, exit_img, 0.8)
-
-# game loop
-run = True
-while run: 
-    
-    if start_button.draw(screen):
-        img = pygame.image.load("resources/background.jpg")
-        screen.blit(img, (0,0))
-    
-    if exit_button.draw(screen):
-        run = False
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            run = False
-    
-    pygame.display.flip()
-
-pygame.quit()
-'''
